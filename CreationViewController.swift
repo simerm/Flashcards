09a0c054619dcf72f1b894555f91a093e0233170
Simerjeet Mudhar <simerkm@gmail.com>
2022-03-18 22:31:58 -0400

    added new features

---
+++ CreationViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+    
+    var flashcardsController: ViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        
+        let questionText = questionTextField.text
+        let answerText = answerTextField.text
+        flashcardsController.updateFlashcard(question: questionText!, answer: answerText!)
+        dismiss(animated:true)
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -16,10 +16,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashcardsController = self
+    }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         questionLabel.isHidden = true;
     }
+    
+
+    func updateFlashcard(question: String, answer: String) {
+        
+        questionLabel.text = question
+        answerLabel.text = answer
+    }
+    
     
 }
 
